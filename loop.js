f1faf2d68f7dcf7fdb9e216c5fd78171eae678d8
suborna33 <152749142+suborna33@users.noreply.github.com>
2025-12-19 21:46:27 +0600

    Loop

---
+++ loop.js
@@ -0,0 +1,104 @@
+// // for...of 
+ 
+//  const numbers = [12, 21, 34, 54, 23, 59, 45];
+
+//  for (const num of numbers) {
+//     console.log(num);
+
+//  }
+
+// //  output:
+// //  12
+// // 21
+// // 34
+// // 54
+// // 23
+// // 59
+// // 45
+
+// const fruits = ['orange', 'apple', 'banana'];
+// for (const fruit of fruits) {
+//     console.log(fruit);
+// }
+// // orange
+// // apple
+// // banana
+
+
+// // while loop
+// let count = 1;
+// while (count <= 5) {
+//     console.log(count);
+//     count++;
+// }
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+
+// let num = 1;
+// while (num <= 3) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// console.log("-------------");
+
+// const hu = [54,5,45,545,4,5,45,45];
+// for (const h of hu) {
+//     console.log(h);
+// }
+
+
+// console.log("-------------");
+
+// let counts = 1;
+// while (counts <= 3) {
+//     console.log(counts);
+//     counts++;
+// }
+
+
+// console.log("-------------");
+
+
+// let n = 50;
+// while (n<=60) {
+//     console.log(n);
+//     n++;
+// }
+
+// console.log("-------------");
+
+
+
+
+
+
+// let num = 1;
+// let sum = 0;
+// while (num<=2) {
+//     console.log(num);
+//     sum = sum + num;
+//     console.log(sum);
+//     num++;
+// }
+
+// 1
+// 1
+// 2
+// 3
+
+
+// console.log('----------------------');
+
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+    if(num % 2 === 0) {
+        console.log('even number', num);
+    }
+    num++;
+}
