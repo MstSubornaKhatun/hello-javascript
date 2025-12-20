b27a7e2b3794cc4b3a3034045ad583b34e401c71
suborna33 <152749142+suborna33@users.noreply.github.com>
2025-12-20 07:34:19 +0600

    even number

--- loop.js
+++ loop.js
@@ -94,9 +94,10 @@
 
 // console.log('----------------------');
 
+// even number
 let num = 1;
 while (num <= 10) {
-    console.log(num);
+    // console.log(num);
     if(num % 2 === 0) {
         console.log('even number', num);
     }
